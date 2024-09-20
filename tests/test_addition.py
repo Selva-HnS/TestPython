bf32b2748fda6fc80e65a2373acc9b4ab1602e8a
Selva-HnS <80045197+Selva-HnS@users.noreply.github.com>
2024-09-20 15:47:55 +0530

    Update test_addition.py

--- tests/test_addition.py
+++ tests/test_addition.py
@@ -5,14 +5,14 @@ from src.math_functions import add
 import HtmlTestRunner
 
 class TestAddition(unittest.TestCase):
-	def test_add(self):
-		self.assertEqual(add(1, 2), 3)
-		self.assertEqual(add(-1, 1), 0)
-		self.assertNotEqual(add(1, 1), 3)
+    def test_add(self):
+        self.assertEqual(add(1, 2), 3)
+        self.assertEqual(add(-1, 1), 0)
+        self.assertNotEqual(add(1, 1), 3)
 
 if __name__ == '__main__':
-	#unittest.main()
-	try:
-		unittest.main(testRunner=HtmlTestRunner.HTMLTestRunner(output='test-reports'))
-	except Exception as e:
-		print(f"An error occurred: {e}")
+    #unittest.main()
+    try:
+        unittest.main(testRunner=HtmlTestRunner.HTMLTestRunner(output='test-reports'))
+    except Exception as e:
+        print(f"An error occurred: {e}")
