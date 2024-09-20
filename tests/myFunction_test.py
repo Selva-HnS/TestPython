21b96af6dcfb83c77924fd0ba77c39b237c5c8f6
Selva-HnS <80045197+Selva-HnS@users.noreply.github.com>
2024-09-20 09:20:20 +0530

    Update myFunction_test.py

--- tests/myFunction_test.py
+++ tests/myFunction_test.py
@@ -1,7 +1,11 @@
 import unittest
+import HtmlTestRunner
 from .context import src
 
 class SumTest(unittest.TestCase):
     def test_Sum(self):
         self.assertEqual(src.Sum(1, 1), 2)
         self.assertTrue(src.Sum(1, 2) == 3)
+        
+if __name__ == '__main__':
+    unittest.main(testRunner=HtmlTestRunner.HTMLTestRunner(output='test-reports'))
