d9265756f06079fbaeee3ba9bd2dae4171036392
Selva-HnS <80045197+Selva-HnS@users.noreply.github.com>
2024-09-20 15:36:02 +0530

    Delete tests/mock_test.py

--- tests/mock_test.py
+++
@@ -1,9 +0,0 @@
-import unittest
-from unittest import mock  # Correct import for Python 3
-from .context import src
-
-class RandomTest(unittest.TestCase):
-    @mock.patch('os.urandom')
-    def test_Random(self, random_mock):
-        random_mock.return_value = 'aaa'
-        self.assertEqual(src.Random(2), '2aaa')
